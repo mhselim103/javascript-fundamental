a46b49e889608d2c5f85ee48bd37c19cad94006a
mhselim103 <mhselin103@gmail.com>
2021-08-01 17:32:54 +0600

    multiple condition added

--- comparison.js
+++ comparison.js
@@ -4,6 +4,10 @@ console.log(5 != 5);
 console.log(5 == 5);
 
 
+// adding two condition 
 
-console.log(studyGood && bcsCadre);
-console.log(studyGood || die);
+console.log("studyGood && bcsCadre");
+
+// fill one condition between two 
+
+console.log("studyGood || die");
---
+++ multiple-condition.js
@@ -0,0 +1,19 @@
+var gotJob = true;
+var moneySaved = 150000;
+var hasFlat = false;
+
+// if (gotJob = true) {
+//     console.log("let's get married");
+
+// if (gotJob = true && moneySaved > 200000) {
+//     console.log("let's get married");
+// }
+// if (gotJob = true || moneySaved > 200000) {
+//     console.log("let's get married");
+// }
+if (gotJob = true && moneySaved > 200000 && hasFlat == true) {
+    console.log("let's get married");
+}
+else{
+    console.log("u have no right to marry ");
+}
