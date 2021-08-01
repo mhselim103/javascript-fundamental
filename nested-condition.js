84dfd9e2180ee1fa4f3dcd0117c23ee5cce49613
mhselim103 <mhselin103@gmail.com>
2021-08-01 18:14:35 +0600

    nested condition added

---
+++ nested-condition.js
@@ -0,0 +1,31 @@
+var biscuit = 30;
+var butterBun = 45;
+var toastBiscuit = 50;
+var packedWell = false;
+var myBudget = 50;
+
+// if (myBudget > toastBiscuit) {
+//     console.log("Toast khabo");
+// }
+// else if (myBudget > butterBun) {
+//     console.log("butterbun khabo");
+// }
+// else if (myBudget > biscuit) {
+//     console.log("biscuit khabo");
+// }
+// else {
+//     console.log("cha de vai");
+// }
+
+if (myBudget > butterBun) {
+    if (packedWell == true) {
+        console.log("accha den");
+    }
+    else {
+        console.log("tor bun tui kha ");
+    }
+}
+
+else {
+    console.log("baler bun khailam na");
+}
